8af18f522652af98223e34b9cdaf40208090c5cc
GreggSanchez <gregg.sanchez@nixie.fr>
2020-11-26 14:56:34 +0100

    feat (router) : add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import logo from './logo.svg';
 import React from 'react'
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Components
 import { Navbar } from './components/Navbar'
+
+// Views
+import { Landing } from './views/Landing'
 
 function App() {
 	return (
 		<div>
 			<Navbar />
-			<div className="content">
-
-			</div>
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Landing} />
+					<Route exact path="/offers" />
+					<Route exact path="/login" />
+					<Route exact path="/register" />
+					<Route exact path="/password/forgot" />
+					<Route exact path="/guide" />
+				</Switch>
+			</Router>
 		</div>
 	);
 }
